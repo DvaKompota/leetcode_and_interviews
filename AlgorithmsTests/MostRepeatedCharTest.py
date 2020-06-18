2f1ac69f842e1de17865b8f9d95a7a3ce68777b6
Sergey Kolokolov <sergey.kolokolov@appzen.com>
2020-06-17 17:53:36 -0700

    Finished MostRepeatedChar

---
+++ AlgorithmsTests/MostRepeatedCharTest.py
@@ -0,0 +1,23 @@
+import unittest
+from Algorithms.MostRepeatedChar import MostRepeatedChar
+
+
+class MostRepeatedCharTest(unittest.TestCase):
+
+    def testMostRepeatedChar(self):
+        given_string = 'qwewrty'
+        expected_result = 'w'
+        actual_result = MostRepeatedChar().most_repeated_char(given_string)
+        self.assertEqual(expected_result, actual_result)
+
+    def testMostRepeatedCharOne(self):
+        given_string = 'q'
+        expected_result = 'q'
+        actual_result = MostRepeatedChar().most_repeated_char(given_string)
+        self.assertEqual(expected_result, actual_result)
+
+    def testMostRepeatedCharDigits(self):
+        given_string = 'qwewrty123334'
+        expected_result = '3'
+        actual_result = MostRepeatedChar().most_repeated_char(given_string)
+        self.assertEqual(expected_result, actual_result)
