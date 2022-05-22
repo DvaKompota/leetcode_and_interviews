bd363f6979ce80d04448d52c41ba37251a3562ab
Sergey Kolokolov <serg.a.bells@gmail.com>
2022-05-22 16:47:00 -0700

    adds TeslaInterview tasks array_of_unique_integers_with_zero_sum() and get_success_rate() with unit tests

--- Tasks/InterviewTasks/TeslaInterview.py
+++ Tasks/InterviewTasks/TeslaInterview.py
@@ -1,6 +1,9 @@
-class TeslaInterview:
+import re
 
-    # codility.com demo task
+
+class CodilityTasks:
+
+    # demo task
     def first_missing_positive(self, nums: [int]) -> int:
         """
         Given an array of integers, find the first missing positive integer.
@@ -19,11 +22,13 @@ class TeslaInterview:
             if i not in nums_set:
                 return i
 
-
-    # codility.com demo task2
+    # demo task 2
     def longest_binary_gap(self, num: int) -> int:
         """
         Find the longest binary gap in a given integer.
+
+        A binary gap within a positive integer num is any maximal sequence of consecutive zeros
+        that is surrounded by ones at both ends in the binary representation of num.
         """
         n_bin = bin(num)
         bin_str = str(n_bin)[2:]
@@ -51,3 +56,60 @@ class TeslaInterview:
                     gap_started = True
                 gap_ended = False
         return longest_gap
+
+    # Tesla Online Coding Assessment – https://app.codility.com/public-link/Tesla-Qa-Automation---Sr-CR--2-2021-1/
+    # task1
+    def array_of_unique_integers_with_zero_sum(self, n: int) -> [int]:
+        """
+        Return an array with length equal to n, that consists of unique integers with zero-sum.
+
+        Array of integers with zero-sum means that the sum of all elements in the array is zero.
+        """
+        if n == 1:
+            return [0]
+        solution = []
+        if n % 2 != 0:
+            solution.append(0)
+        for i in range(1, n//2+1):
+            solution.append(-i)
+            solution.append(i)
+        return solution
+
+    # task2
+    def get_success_rate(self, tests: [str], results: [str]) -> int:
+        """
+        Return the success rate of the tests, given 2 arrays of strings, containing tests names and their results.
+
+        Tests array contains unique tests names, consisting of parts: module name, test group number and test letter.
+        If the group of tests has only one test, the test letter is omitted.
+        Otherwise the test letters are lowercase english letters in alphabetical order, starting from 'a'.
+        Examples of test names: "test1a", "test2", "codility1b", "codility1c", "recursion3", etc.
+        Test names in the array are not sorted by their groups: i.e. ["test1a", "test2", "test1b", "test3", "test1c"]
+
+        Test results are strings: "OK", "Wrong answer", "Time limit exceeded", "Runtime error".
+        Only "OK" ones are considered as successful tests.
+
+        Success rate is the number of passed groups of tests divided by the total number of groups.
+        A group of tests is considered as passed if all tests in the group are passed.
+        For example, with:
+        tests = ["codility1", "codility3", "codility2", "codility4b", "codility4a"], and
+        results = ["Wrong answer", "OK", "OK", "Runtime error", "OK"], then
+        the success rate is 50, because there are 4 groups of tests, and only groups 2 and 3 have passed all the tests.
+        """
+        grouped_results = {}
+        for i in range(len(tests)):
+            group_no = re.search(r'\d+', tests[i]).group()
+            if group_no not in grouped_results:
+                grouped_results[group_no] = []
+            grouped_results[group_no].append(results[i])
+        total_groups = len(grouped_results)
+        passed_groups = 0
+        for key, value in grouped_results.items():
+            group_failed = False
+            for res in value:
+                if res != "OK":
+                    group_failed = True
+                    break
+            if not group_failed:
+                passed_groups += 1
+        return int(passed_groups / total_groups * 100)
--- UnitTests/InterviewTests/TeslaInterviewTest.py
+++ UnitTests/InterviewTests/TeslaInterviewTest.py
@@ -1,136 +1,255 @@
 import unittest
-from Tasks.InterviewTasks.TeslaInterview import TeslaInterview
+from Tasks.InterviewTasks.TeslaInterview import CodilityTasks
 
 
-class TeslaInterviewCodilityFirstMissingPositiveTest(unittest.TestCase):
+class TeslaInterviewCodilityTasksFirstMissingPositiveTest(unittest.TestCase):
 
-    def testTeslaInterviewCodilityFirstMissingPositive1(self):
+    def testTeslaInterviewCodilityTasksFirstMissingPositive1(self):
         given_array = [3, 4, -1, 1]
         expected_result = 2
-        actual_result = TeslaInterview().first_missing_positive(given_array)
+        actual_result = CodilityTasks().first_missing_positive(given_array)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityFirstMissingPositive2(self):
+    def testTeslaInterviewCodilityTasksFirstMissingPositive2(self):
         given_array = [1, 2, 0]
         expected_result = 3
-        actual_result = TeslaInterview().first_missing_positive(given_array)
+        actual_result = CodilityTasks().first_missing_positive(given_array)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityFirstMissingPositive3(self):
+    def testTeslaInterviewCodilityTasksFirstMissingPositive3(self):
         given_array = [1, 3, 6, 4, 1, 2]
         expected_result = 5
-        actual_result = TeslaInterview().first_missing_positive(given_array)
+        actual_result = CodilityTasks().first_missing_positive(given_array)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityFirstMissingPositive4(self):
+    def testTeslaInterviewCodilityTasksFirstMissingPositive4(self):
         given_array = [1, 2, 3]
         expected_result = 4
-        actual_result = TeslaInterview().first_missing_positive(given_array)
+        actual_result = CodilityTasks().first_missing_positive(given_array)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityFirstMissingPositive5(self):
+    def testTeslaInterviewCodilityTasksFirstMissingPositive5(self):
         given_array = [-1, -3]
         expected_result = 1
-        actual_result = TeslaInterview().first_missing_positive(given_array)
+        actual_result = CodilityTasks().first_missing_positive(given_array)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityFirstMissingPositive6(self):
+    def testTeslaInterviewCodilityTasksFirstMissingPositive6(self):
         given_array = [7, 8, 9, 11, 12]
         expected_result = 1
-        actual_result = TeslaInterview().first_missing_positive(given_array)
+        actual_result = CodilityTasks().first_missing_positive(given_array)
         self.assertEqual(expected_result, actual_result)
 
 
-class TeslaInterviewCodilityLongestBinaryGapTest(unittest.TestCase):
+class TeslaInterviewCodilityTasksLongestBinaryGapTest(unittest.TestCase):
 
-    def testTeslaInterviewCodilityLongestBinaryGap1(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap1(self):
         given_integer = 9
         expected_result = 2
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap2(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap2(self):
         given_integer = 529
         expected_result = 4
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap3(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap3(self):
         given_integer = 20
         expected_result = 1
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap4(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap4(self):
         given_integer = 15
         expected_result = 0
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap5(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap5(self):
         given_integer = 32
         expected_result = 0
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap6(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap6(self):
         given_integer = 1041
         expected_result = 5
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap7(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap7(self):
         given_integer = 328
         expected_result = 2
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap8(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap8(self):
         given_integer = 1162
         expected_result = 3
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap9(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap9(self):
         given_integer = 51712
         expected_result = 2
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap10(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap10(self):
         given_integer = 66561
         expected_result = 9
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap11(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap11(self):
         given_integer = 6291457
         expected_result = 20
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap12(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap12(self):
         given_integer = 74901729
         expected_result = 4
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap13(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap13(self):
         given_integer = 805306373
         expected_result = 25
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap14(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap14(self):
         given_integer = 1376796946
         expected_result = 5
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
         self.assertEqual(expected_result, actual_result)
 
-    def testTeslaInterviewCodilityLongestBinaryGap15(self):
+    def testTeslaInterviewCodilityTasksLongestBinaryGap15(self):
         given_integer = 1610612737
         expected_result = 28
-        actual_result = TeslaInterview().longest_binary_gap(given_integer)
+        actual_result = CodilityTasks().longest_binary_gap(given_integer)
+        self.assertEqual(expected_result, actual_result)
+
+
+class TeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSumTest(unittest.TestCase):
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum1(self):
+        given_integer = 1
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum2(self):
+        given_integer = 2
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum3(self):
+        given_integer = 3
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum4(self):
+        given_integer = 4
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum9(self):
+        given_integer = 9
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum10(self):
+        given_integer = 10
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum999(self):
+        given_integer = 999
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+    def testTeslaInterviewCodilityTasksArrayOfUniqueIntegersWithZeroSum1000(self):
+        given_integer = 1000
+        actual_result = CodilityTasks().array_of_unique_integers_with_zero_sum(given_integer)
+        self.assertEqual(len(actual_result), given_integer)  # length of the result array is equal to the given integer
+        self.assertEqual(len(actual_result), len(set(actual_result)))  # all integers in the result array are unique
+        self.assertEqual(sum(actual_result), 0)  # sum of all integers in the result array is equal to zero
+
+
+class TeslaInterviewCodilityTasksGetSuccessRateTest(unittest.TestCase):
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate1(self):
+        tests_array = ["codility1", "codility3", "codility2", "codility4b", "codility4a"]
+        results_array = ["Wrong answer", "OK", "OK", "Runtime error", "OK"]
+        expected_result = 50
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate2(self):
+        tests_array = ["test1a", "test2", "test1b", "test1c", "test3"]
+        results_array = ["Wrong answer", "OK", "Runtime error", "OK", "Time limit exceeded"]
+        expected_result = 33
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate3(self):
+        tests_array = ["test1c", "test2a", "test1a", "test2b", "test3", "test4b", "test1b", "test6", "test5", "test4a"]
+        results_array = ["Wrong answer", "OK", "Runtime error", "OK", "Time limit exceeded", "OK", "Runtime error",
+                         "OK", "Time limit exceeded", "OK"]
+        expected_result = 50
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate4(self):
+        tests_array = ["test1b", "test2", "test1a"]
+        results_array = ["Time limit exceeded", "Runtime error", "OK"]
+        expected_result = 0
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate5(self):
+        tests_array = ["test1b", "test2a", "test1a", "test2b", "test3"]
+        results_array = ["OK", "OK", "OK", "Wrong answer", "OK"]
+        expected_result = 66
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate6(self):
+        tests_array = ["test1b", "test2", "test1a"]
+        results_array = ["OK", "OK", "OK"]
+        expected_result = 100
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate7(self):
+        tests_array = ["test1"]
+        results_array = ["OK"]
+        expected_result = 100
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
+        self.assertEqual(expected_result, actual_result)
+
+    def testTeslaInterviewCodilityTasksGetSuccessRate8(self):
+        tests_array = ["test1"]
+        results_array = ["Runtime error"]
+        expected_result = 0
+        actual_result = CodilityTasks().get_success_rate(tests_array, results_array)
         self.assertEqual(expected_result, actual_result)
 
 
